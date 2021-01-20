01f3c687d2ecb5cda10a5dc0913aa3add42a38e3
Daulet-raect <dauletjenisov@gmail.com>
2021-01-21 00:58:31 +0600

    Updates

---
+++ precache-manifest.721d71b8bc054531c9fad0674864fc26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1284638398dd2eeb628b5f093648501d",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "3d63158a4b34a5405461",
+    "url": "/weather-app-react/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "6bca0044b4a37ca5fa5e",
+    "url": "/weather-app-react/static/css/main.434534b5.chunk.css"
+  },
+  {
+    "revision": "3d63158a4b34a5405461",
+    "url": "/weather-app-react/static/js/2.e3489f9b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/weather-app-react/static/js/2.e3489f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bca0044b4a37ca5fa5e",
+    "url": "/weather-app-react/static/js/main.2ef587e5.chunk.js"
+  },
+  {
+    "revision": "99ccd5df7b00e6483a6d",
+    "url": "/weather-app-react/static/js/runtime-main.6b239146.js"
+  },
+  {
+    "revision": "560c529bf84f285840515cb2d21af91a",
+    "url": "/weather-app-react/static/media/back.560c529b.jpg"
+  }
+]);
--- src/reducers/AppReducer.js
+++
@@ -1,6 +0,0 @@
-export const AppReducer = (state = {initialized: false}, action) => {
-    switch (action.type) {
-
-        default: return state
-    }
-}
